65b05c810b334ac009835d1e64009bb3f1b779fa
nayoungjin39 <sksmssidxndx@gmail.com>
2025-07-08 10:59:43 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.cpp
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.cpp
@@ -5,8 +5,10 @@ using namespace std;
 
 int solution(int n) {
     int answer = 0;
-    for (int a = 2; a <= n; a += 2) {
-    answer += a;
+    for(int a = 2; a <= n; a += 2)
+    {
+        answer += a;
     }
+          
     return answer;
 }
